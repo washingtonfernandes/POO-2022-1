08a2b0d621d473b9f1ca301758c5f62110e016f0
washingtonfernandes <washington.fernandes@rede.ulbra.br>
2022-03-25 19:37:39 -0300

    aula5

--- AULA2/Entities/Livro.cs
+++ AULA2/Entities/Livro.cs
@@ -1,9 +1,24 @@
 namespace aula2.Entities
 {
-    public class Livro
+    public class Triangulo
     {
     
+            public double area;
+            public double altura;
 
+        public void lerDados()
+    {
+        
+    }
+        public void calcular()
+    {
+
+
+    }
+        public void exibir()
+    {
+
+    }
 
 
     }
